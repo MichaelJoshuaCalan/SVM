a70d884c368cca67a9e7b56a777c07c11687c110
MichaelJoshuaCalan <mr.swabe.xx12345@gmail.com>
2024-03-28 21:58:22 +0800

    new update

--- emotion_prediction/emotion_prediction/urls.py
+++ emotion_prediction/emotion_prediction/urls.py
@@ -22,10 +22,17 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('home', views.homeprivate, name='privhome'),
 
     path('register', views.register, name='register'),
+    # path('registerr', views.register_error, name='registerr'),
+    # path('registersuccess', views.register_success, name='registersuccess'),
+    
+
+
     path('login', views.user_login, name='login'),
     path('loginerror', views.user_login_error, name='loginerror'),
+    path("logout", views.user_logout, name="logout"),
 
 
     path('per-paragraph', views.paragraph, name='paragraph'),
@@ -33,7 +40,8 @@ urlpatterns = [
     path('change', views.change, name='change'),
     path('profile', views.profile, name='profile'),
     
- path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
@@ -42,6 +50,8 @@ urlpatterns = [
     
     path('change-password/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     path('password-change-done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
+    path('result/download', views.download_result, name="download-result"),
     
     
     
--- emotion_prediction/setup/views.py
+++ emotion_prediction/setup/views.py
@@ -2,31 +2,93 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login as auth_login,views as auth_views
+from .forms import Userform, StoryForm
+from .models import AnalyzedStory
+from django.contrib.auth import authenticate, logout, login as auth_login,views as auth_views
+from django.contrib.auth.decorators import login_required
+
+# PDF DOWNLAOD
+from django.http import HttpResponse, HttpResponseServerError
+from django.template.loader import get_template
+import io
+import xhtml2pdf.pisa as pisa
+from django.core.paginator import Paginator
 
 def home(request):
     return render(request, 'index.html')
 
+def homeprivate(request):
+    return render(request, 'privateindex.html')
 
 def register(request):
-    if request.method == 'POST':
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
-        confirm_password = request.POST['confirm_password']
+    form = Userform
+    success = False
 
-        # Check if passwords match
-        if password != confirm_password:
-            return HttpResponse("Passwords do not match")
+    if request.method == "POST":
+        form = Userform(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Registration success!")
+            success = True
+            return redirect('login')
+        else:
+            messages.error(request, "Form is not valid")
+            success = False
+    
+    else:
+        form = Userform()
+    
+    context = {
+        "form": form, 
+        "success": success
+        }
+    
+    return render(request, 'sign_up.html', context)    
+    
+    # if request.method == 'POST':
+    #     first_name = request.POST['first_name']
+    #     last_name = request.POST['last_name']
+    #     username = request.POST['username']
+    #     email = request.POST['email']
+    #     password = request.POST['password']
+    #     confirm_password = request.POST['confirm_password']
 
-        # Create a new user
-        my_user = User.objects.create_user(username=username,email=email, password=password, first_name=first_name, last_name=last_name)
+    #     # Check if passwords match
+    #     if password != confirm_password:
+    #         return redirect(register_error)
+
+    #     # Create a new user
+    #     my_user = User.objects.create_user(username=username,email=email, password=password, first_name=first_name, last_name=last_name)
         
-        return HttpResponse("User created successfully")
+    #     return redirect(register_success)
   
-    return render(request, 'sign_up.html')
+    # return render(request, 'sign_up.html')
+
+
+# def register_error(request):
+#     if request.method == 'POST':
+#         first_name = request.POST['first_name']
+#         last_name = request.POST['last_name']
+#         username = request.POST['username']
+#         email = request.POST['email']
+#         password = request.POST['password']
+#         confirm_password = request.POST['confirm_password']
+
+#         # Check if passwords match
+#         if password != confirm_password:
+#             return redirect(register_error)
+
+#         # Create a new user
+#         my_user = User.objects.create_user(username=username,email=email, password=password, first_name=first_name, last_name=last_name)
+        
+#         return redirect(register_success)
+  
+#     return render(request, 'sign_up_error.html')
+
+# def register_success(request):
+#     return render(request, "success_reg.html")
+
+
 
 
 def user_login(request):
@@ -44,6 +106,11 @@ def user_login(request):
     return render(request, 'logiin.html')
 
 
+def user_logout(request):
+    logout(request)
+    return redirect("home")
+
+
 def user_login_error(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -56,64 +123,115 @@ def user_login_error(request):
         else:
             return redirect(user_login_error)
 
-    return render(request, 'login_error.html')
+    return render(request, 'logiin_error.html')
 
 
 
-from django.contrib.auth.forms import PasswordResetForm
+# from django.contrib.auth.forms import PasswordResetForm
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+@login_required(login_url='/login')
 def paragraph(request):
-    return render(request, 'emoji.html')
+    form = StoryForm()
+    success = False
 
-def whole(request):
-    return render(request, 'emoji-whole.html')
+    recent_user_story = None  
 
-def profile(request):
-    user = request.user  # Replace this with your user retrieval logic
+    if request.method == 'POST':
+        form = StoryForm(request.POST)
+        if form.is_valid():
+            form.save(request)
+            user_stories = AnalyzedStory.objects.filter(user=request.user.users).order_by('-id')
+            recent_user_story = user_stories.first()
+            success = True
 
     context = {
-        'user': user,
+        'form': form, 
+        'analyzed_story': recent_user_story, 
+        "success": success
+        }
+    
+    return render(request, 'emoji.html', context)
+
+
+@login_required(login_url='/login')
+def profile(request):
+    logged_user = request.user.users  
+    user_stories = AnalyzedStory.objects.filter(user=logged_user).order_by('-id')
+
+    # Paginate the user stories
+    paginator = Paginator(user_stories, 7)  
+    page_number = request.GET.get('page')
+    user_stories_paginated = paginator.get_page(page_number)
+
+    context = {
+        'logged_user': logged_user,
+        'user_stories': user_stories_paginated
     }
-    return render(request, 'profile.html')
+    return render(request, 'profile.html', context)
 
 def change(request):
-    if request.method== 'POST':
-        old_password = request.POST['old_password']
-        new_password = request.POST['new_password']
-        confirm_password = request.POST['confirm_password']
+   
+        
 
     
     return render(request,'changepass.html')
+
+@login_required(login_url='/login')
+def whole(request):
+    form = StoryForm()
+    success = False
+    recent_user_story = None
+    content = None
+
+    if request.method == 'POST':
+        form = StoryForm(request.POST, request.FILES)
+
+        uploaded_file = request.FILES.get('file')
+        
+        if uploaded_file:
+            file_content = uploaded_file.read().decode('utf-8')
+            content = file_content
+            form.data = form.data.copy()
+            form.data['story'] = file_content # store the decoded story from the uploaded file to story field
+            print(content)
+
+        if form.is_valid():
+            form.save(request)
+            success = True
+            user_stories = AnalyzedStory.objects.filter(user=request.user.users).order_by('-id')
+            recent_user_story = user_stories.first()
+        else:
+            print("Form is not valid:", form.errors)
+
+    context = {
+        'form': form,
+        'analyzed_story': recent_user_story,
+        'success': success,
+        'content': content, 
+    }
+
+    return render(request, 'emoji-whole.html', context)
+
+
+# download result
+def download_result(request):
+    logged_user = request.user.users  
+    user_stories = AnalyzedStory.objects.filter(user=logged_user).order_by('-id')
+    recent_user_story = user_stories.first()
+
+    context = {
+        'logged_user': logged_user,
+        'user_stories': user_stories,
+        'recent_user_story': recent_user_story
+    }
+
+    # Download as PDF
+    template = get_template('registration/pdf.html')
+    html = template.render(context)
+    pdf_file = io.BytesIO()
+    pisa.CreatePDF(html, dest=pdf_file, orientation='Portrait', pagesize='A4')
+    
+    # Create and return the PDF response
+    response = HttpResponse(pdf_file.getvalue(), content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="result.pdf"'
+    return response
